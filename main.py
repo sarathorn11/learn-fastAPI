ca9de4352a682e17abc7332b1854ff30b2e47097
sarath.orn <sarath.orn@z1fsc.com>
2023-02-28 19:48:09 +0700

    post method, set type of data, optional data, class

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 import uvicorn
 app = FastAPI()
 
-
-@app.get("/")
-def index():
-    return {'data': {'blog list'}}
+# blog?limit=10 & public=boolean you can write like this in URL
+@app.get("/blog")
+def index(limit=10,public:bool=True,sort:Optional[str]=None): #value of public is boolean because you want to check true or false
+    if public:
+        return {'data': f'{limit} are public from list of blog'}
+    else:
+        return {'data': f'{limit} from list of blog'}
 
 # why I put unpublished functions on show function.
 #  because in fastAPI it exicute code line by line
@@ -23,6 +28,35 @@ def show(id:int):
 def comment(id:int):
     # fetch a blog with comment
     return {'data': {'1','2'}}
+
+
+class Blog(BaseModel):
+    title:str
+    body:str
+    published:Optional[bool]
+
+@app.post("/blog")
+def create_blog(request:Blog):
+    return {'data':f"blog have title is {request.title} and have body {request.body} created "}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
