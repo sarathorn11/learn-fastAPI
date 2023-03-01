241d86da1771fd78c4241de4c68410d9d8bc41a4
sarath.orn <sarath.orn@z1fsc.com>
2023-03-01 14:18:34 +0700

    CRUD

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends,status, Response, HTTPException
 from . import schemas,models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -15,10 +15,46 @@ def get_db():
     finally:
         db.close()
 
-@app.post('/blog')
+@app.post('/blog',status_code=status.HTTP_201_CREATED)
 def create(request:schemas.Blog,db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+@app.get('/blog')
+def all(db:Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+@app.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id,db:Session = Depends(get_db)):
+    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+
+    db.commit()
+    return 'done'
+
+@app.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED)
+def update(id, request:schemas.Blog ,db:Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'blog with the id {id} is not available')
+    blog.update({'title':request.title, 'body':request.body})
+    db.commit()
+    return 'updated'
+
+
+
+@app.get('/blog/{id}',status_code=200)
+def show(id,response:Response,db:Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        # way 1-------------------
+        # response.status_code= status.HTTP_404_NOT_FOUND
+        # return {'details': f"Blog with the id {id} not available"}
+        # way2------------------------
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                detail=f"Blog with the id {id} not available")
+
+    return blog
