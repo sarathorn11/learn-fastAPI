2fabee0118f4e6e5e6cdfb0edb3bb19b508cee63
sarath.orn <sarath.orn@z1fsc.com>
2023-03-01 14:41:44 +0700

    add comment

--- blog/main.py
+++ blog/main.py
@@ -14,6 +14,8 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
 # add blog by id by Sarath on 01/03/2023
 @app.post('/blog',status_code=status.HTTP_201_CREATED)
 def create(request:schemas.Blog,db: Session = Depends(get_db)):
@@ -22,18 +24,27 @@ def create(request:schemas.Blog,db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+
 # get all blog by id by Sarath on 01/03/2023
 @app.get('/blog')
 def all(db:Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
+
+
 # delete blog by id by Sarath on 01/03/2023
 @app.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT)
 def destroy(id,db:Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    blog =db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'blog with the id {id} is not available')
+    blog.delete(synchronize_session=False)
 
     db.commit()
-    return 'done'
+    return 'deleted'
+
+
 # update blog by id by Sarath on 01/03/2023
 @app.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED)
 def update(id, request:schemas.Blog ,db:Session = Depends(get_db)):
