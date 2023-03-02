3901d0e47fe6c46e69fa0ca7212871bd6b6f99e8
sarath.orn <sarath.orn@z1fsc.com>
2023-03-02 13:14:58 +0700

    2 oauth

--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends,status,HTTPException
 from .. import schemas,database, models,token
 from sqlalchemy.orm import Session
 from ..hashing import Hash
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter(
     tags=['Authentication']
@@ -9,7 +10,7 @@ router = APIRouter(
 
 
 @router.post('/login')
-def login(request:schemas.Login, db: Session =  Depends(database.get_db)):
+def login(request:OAuth2PasswordRequestForm = Depends(), db: Session =  Depends(database.get_db)):
     user = db.query(models.User).filter(models.User.email == request.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter,Depends,status
 from typing import List
-from .. import  schemas, database
+from .. import  schemas, database,oauth2
 from sqlalchemy.orm import Session
 from ..repository import blog
 
@@ -13,30 +13,30 @@ get_db = database.get_db
 
 # get all blog by id by Sarath on 01/03/2023
 @router.get('/',response_model=List[schemas.ShowTitleBlog]) #we can remove response_model=List[schemas.ShowTitleBlog] or change in schemas if we want to see all data
-def all(db:Session = Depends(get_db)):
+def all(db:Session = Depends(get_db),get_current_user: schemas.User= Depends(oauth2.get_current_user)):
     return blog.get_all(db)
 
 
 # add blog by id by Sarath on 01/03/2023
 @router.post('/',status_code=status.HTTP_201_CREATED)
-def create(request:schemas.Blog,db: Session = Depends(get_db)):
+def create(request:schemas.Blog,db: Session = Depends(get_db),get_current_user: schemas.User= Depends(oauth2.get_current_user)):
     return blog.create(request,db)
 
 
 
 # delete blog by id by Sarath on 01/03/2023
 @router.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT)
-def destroy(id:int,db:Session = Depends(get_db)):
+def destroy(id:int,db:Session = Depends(get_db),get_current_user: schemas.User= Depends(oauth2.get_current_user)):
     return blog.destroy(id,db)
 
 
 # update blog by id by Sarath on 01/03/2023
 @router.put('/{id}',status_code=status.HTTP_202_ACCEPTED)
-def update(id:int, request:schemas.Blog ,db:Session = Depends(get_db)):
+def update(id:int, request:schemas.Blog ,db:Session = Depends(get_db),get_current_user: schemas.User= Depends(oauth2.get_current_user)):
     return blog.update(id,request,db)
 
 
 # get blog by id by Sarath on 01/03/2023
 @router.get('/{id}',status_code=200,response_model=schemas.ShowBlog)
-def show(id:int,db:Session = Depends(get_db)):
+def show(id:int,db:Session = Depends(get_db),get_current_user: schemas.User= Depends(oauth2.get_current_user)):
     return blog.show(id,db)
